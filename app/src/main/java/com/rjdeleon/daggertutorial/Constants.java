ccd859c09aa41727b7e3669c53c5e96caf4396f1
pabs <pabs@alink-group.co.jp>
2018-10-02 06:57:34 +0800

    Setup main activity and add more dependencies

--- app/src/main/java/com/rjdeleon/daggertutorial/Constants.java
+++ app/src/main/java/com/rjdeleon/daggertutorial/Constants.java
@@ -3,4 +3,6 @@ package com.rjdeleon.daggertutorial;
 public class Constants {
 
     public static final String LOG_TAG = "Dagger_Tutorial";
+
+    public static final String BASE_URL = "https://randomuser.me";
 }
--- app/src/main/java/com/rjdeleon/daggertutorial/MainActivity.java
+++ app/src/main/java/com/rjdeleon/daggertutorial/MainActivity.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.rjdeleon.daggertutorial.adapter.RandomUserAdapter;
 import com.rjdeleon.daggertutorial.interfaces.RandomUsersApi;
 import com.rjdeleon.daggertutorial.model.RandomUsers;
 import com.rjdeleon.daggertutorial.tutorial_part1.model.BattleOfBastards;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +31,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+
+        Timber.plant(new Timber.DebugTree());
+
+        HttpLoggingInterceptor httpLoggingInterceptor = new
+                HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                Timber.i(message);
+            }
+        });
+
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient()
+                .newBuilder()
+                .addInterceptor(httpLoggingInterceptor)
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        populateUsers();
     }
 
     private void initViews() {
